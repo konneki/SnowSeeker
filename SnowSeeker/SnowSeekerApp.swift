89de63ed3c8279722bcf5e7820ebcfcb26777a49
Konneki <konnekdaniel@gmail.com>
2023-03-07 20:04:27 +0100

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Daniel Konnek on 7.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
